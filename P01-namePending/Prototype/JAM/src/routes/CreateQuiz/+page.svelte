6ffe39c012269dcdf393028187ea81e7cd3d1341
Bisma Nawaz <bismnawaz@gmail.com>
2023-11-07 20:09:33 +0500

    Create new quiz and title page

---
+++ P01-namePending/Prototype/JAM/src/routes/CreateQuiz/+page.svelte
@@ -0,0 +1,68 @@
+<script></script>
+
+<main>
+	<body>
+		<div class="box">
+			<div class="container">
+				<div class="container mt-4 mb-5 d-flex justify-content-center" id="inside-box">
+					<div class="text-center">
+						<p>
+							<button type="button" class="btn btn-primary btn-block" id="newQuiz"
+								>Create a new quiz</button
+							>
+						</p>
+						<p>
+							<button type="button" class="btn btn-primary btn-block" id="quizHistory"
+								>See quiz history</button
+							>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</main>
+
+<style>
+	body {
+		background: #7801a8;
+	}
+	.container {
+		margin-top: 7rem;
+	}
+	#inside-box {
+		width: 30rem;
+		height: 20rem;
+		background: #c49eff;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		border-radius: 51px;
+		padding: 15rem;
+		padding-top: 20rem;
+	}
+
+	.btn-primary {
+		background-color: #f0e9e9;
+		border: None;
+		color: black;
+		font-family: 'Jeju Gothic', sans-serif;
+		word-wrap: break-word;
+	}
+
+	#newQuiz {
+		font-size: 36px;
+		font-family: 'Jeju Gothic', sans-serif;
+		font-weight: 400;
+		text-align: center;
+		margin-top: -18rem;
+	}
+	#quizHistory {
+		font-size: 36px;
+		font-family: 'Jeju Gothic', sans-serif;
+		font-weight: 400;
+		margin-top: -2rem;
+	}
+	.btn {
+		width: 20rem;
+		height: 6rem;
+	}
+</style>
---
+++ P01-namePending/Prototype/JAM/src/routes/QuizTitle/+page.svelte
@@ -0,0 +1,87 @@
+<script></script>
+
+<main>
+	<body>
+		<div class="container">
+			<h1 class="quiz">Quiz</h1>
+			<p class="title">Start by giving your quiz a name!</p>
+			<form>
+				<div class="form-group">
+					<!-- <label for="Title"></label> -->
+					<input type="text" class="form-control" id="quizTitle" placeholder="Enter title" />
+				</div>
+				<div class="form-group">
+					<!-- <label for="Title"></label> -->
+					<input
+						type="text"
+						class="form-control"
+						id="participants"
+						placeholder="Number of participants"
+					/>
+				</div>
+			</form>
+			<div class="btn-group col-xs-3" role="group">
+				<p><button type="button" class="btn btn-primary btn-block" id="go">Go</button></p>
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+				&nbsp; &nbsp; &nbsp; &nbsp;
+				<p>
+					<button type="button" class="btn btn-primary btn-block btn-space" id="cancel"
+						>Cancel</button
+					>
+				</p>
+			</div>
+		</div>
+	</body>
+</main>
+
+<style>
+	body {
+		background: #7801a8;
+	}
+	.quiz {
+		color: white;
+		font-size: 64px;
+		font-family: JejuGothic;
+		word-wrap: break-word;
+		padding-top: 6rem;
+	}
+	.title {
+		padding-top: 2rem;
+		color: white;
+		font-size: 30px;
+		font-family: JejuGothic;
+		word-wrap: break-word;
+	}
+	#quizTitle {
+		border: None;
+		padding: 0.75rem;
+		background-color: #f0e9e9;
+		margin-bottom: 2rem;
+	}
+	#participants {
+		border: None;
+		padding: 0.75rem;
+		background-color: #f0e9e9;
+		margin-bottom: 2rem;
+	}
+	#go {
+		width: 6rem;
+		padding: 0.75rem;
+		border: None;
+		background: #c49eff;
+		border-radius: 12px;
+	}
+	#cancel {
+		width: 6rem;
+		background: #c70000;
+		border-radius: 12px;
+		border: None;
+		padding: 0.75rem;
+	}
+</style>
