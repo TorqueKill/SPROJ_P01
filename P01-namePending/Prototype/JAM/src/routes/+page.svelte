c7d7897e6d856482c28e6268b002df762178a6ca
rafaelums <118612665+rafaelums@users.noreply.github.com>
2023-11-07 15:52:20 +0500

    Add files via upload

--- P01-namePending/Prototype/JAM/src/routes/+page.svelte
+++ P01-namePending/Prototype/JAM/src/routes/+page.svelte
@@ -1,2 +1,34 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="js">
+
+    // import { onMount } from 'svelte';
+    import io from 'socket.io-client';
+    const socket = io('http://localhost:3000');
+    
+
+    // make a button for creating game
+
+    // onMount(() => {
+        
+    //     testButton = () => {
+    //         socket.emit('test', 'test');
+    //     }
+    // });
+
+    const testButton = () => {
+        socket.emit('test', 'test');
+        socket.emit('hello', 'hello world');
+        console.log("hello test");
+    };
+    // socket.emit('test', 'test');
+
+    
+
+
+</script>
+
+<button on:click={testButton}>test</button>
+
+
