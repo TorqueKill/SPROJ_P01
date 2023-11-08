a4de9f024daa4ffe6d3080ad395a25d0d702f842
rafaelums <118612665+rafaelums@users.noreply.github.com>
2023-11-08 17:48:52 +0500

    Add files via upload

--- P01-namePending/Prototype/JAM/src/routes/+page.svelte
+++ P01-namePending/Prototype/JAM/src/routes/+page.svelte
@@ -5,24 +5,39 @@
 
     // import { onMount } from 'svelte';
     import io from 'socket.io-client';
-    const socket = io('http://localhost:3000');
+    export const socket = io('http://localhost:3000',);
+    import { writable } from 'svelte/store';
+
+    export const socketStore = writable(socket);
+    
+    socketStore.set(socket);
+    
+    import {goto} from '$app/navigation';
     
 
-    // make a button for creating game
-
-    // onMount(() => {
-        
-    //     testButton = () => {
-    //         socket.emit('test', 'test');
-    //     }
-    // });
+    
 
     const testButton = () => {
+        
         socket.emit('test', 'test');
-        socket.emit('hello', 'hello world');
+        // socket.emit('hello', 'hello world');
         console.log("hello test");
     };
-    // socket.emit('test', 'test');
+
+    const createGame = () => {
+        let player_min = 2;
+        let player_max = 4;
+        socket.emit('createGame', {player_min, player_max});
+        console.log("createGame");
+
+        socket.on('gameCreated', (room_id) => {
+            console.log("Game created with room_id: ",room_id);
+            goto(`/waitLobby`, {replaceState: true, state: {socket: socket, room_id: room_id}});
+
+
+        });
+    };
+    
 
     
 
@@ -30,5 +45,13 @@
 </script>
 
 <button on:click={testButton}>test</button>
+<!-- add button with a normal javscript function that emits a socket event -->
+<!-- <button on:click={() => socket.emit('test', 'test')}>test2</button>
+<button id="test_vanilla_Button">test3</button> -->
+<button on:click={createGame}>Create Game</button>
+```
+
+
+
 
 
