ea7dba2da4d5242d0e0d86bb5eb468ea04ed146f
rafaelums <118612665+rafaelums@users.noreply.github.com>
2023-11-08 17:53:52 +0500

    Add files via upload

---
+++ P01-namePending/Prototype/JAM/src/routes/waitLobby/+page.svelte
@@ -0,0 +1,23 @@
+<h1>Waiting for all Players to Join</h1>
+
+<script lang="js">
+	// import { page } from "$app/stores";
+    // import {socketStore} from './socketStore.js'
+    // import {state} from '$app/stores';
+	
+    // import {socket} from './routes/page.svelte';
+
+    // const socket = history.state.socket;
+    // const room_id = history.state.room_id;
+
+    // const socket = page.params.socket;
+    // const room_id = page.params.room_id;
+
+    // const socket = $page.params.socket;
+    // const room_id = $page.params.room_id;
+
+    // socket.on('playerJoined', (player) => {
+    //     console.log("player joined: ", player);
+    // });
+
+</script>
